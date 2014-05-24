37fa09e9dfb1263035577aba4cafdc31c05b6e68
Shigenori Taya <shigenori.taya@gmail.com>
2014-05-25 02:13:48 +0900

    adding data from https://github.com/johnmyleswhite/ML_for_Hackers

---
+++ 06_regularization_text_regression/6_3_text_regression.R
@@ -0,0 +1,56 @@
+
+setwd("./Github/ML_for_Hackers/06_regularization/")
+
+library('ggplot2')
+library('tm')
+library('glmnet')
+
+# read csv and create Document Term Matrix
+ranks <- read.csv('data/oreilly.csv', stringsAsFactors = FALSE)
+
+documents <- data.frame(Text = ranks$Long.Desc.)
+row.names(documents) <- 1:nrow(documents)
+
+corpus <- Corpus(DataframeSource(documents))
+corpus <- tm_map(corpus, tolower)
+corpus <- tm_map(corpus, stripWhitespace)
+corpus <- tm_map(corpus, removeWords, stopwords('english'))
+
+dtm <- DocumentTermMatrix(corpus)
+
+# learn on training data and cross-validate on test data
+# with varying lambda, 50 iterations
+x <- as.matrix(dtm)
+y <- rev(1:100)
+
+set.seed(1)
+
+performance <- data.frame()
+
+for (lambda in c(0.1, 0.25, 0.5, 1, 2, 5))
+{
+  for (i in 1:50)
+  {
+    indices <- sample(1:100, 80)
+
+    training.x <- x[indices, ]
+    training.y <- y[indices]
+
+    test.x <- x[-indices, ]
+    test.y <- y[-indices]
+
+    glm.fit <- glmnet(training.x, training.y)
+    predicted.y <- predict(glm.fit, test.x, s = lambda)
+    rmse <- sqrt(mean((predicted.y - test.y) ^ 2))
+
+    performance <- rbind(performance,
+                         data.frame(Lambda = lambda,
+                                    Iteration = i,
+                                    RMSE = rmse))
+  }
+}
+
+ggplot(performance, aes(x = Lambda, y = RMSE)) +
+  stat_summary(fun.data = 'mean_cl_boot', geom = 'errorbar') +
+  stat_summary(fun.data = 'mean_cl_boot', geom = 'point')
+
